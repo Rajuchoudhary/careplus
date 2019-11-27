a012bea81604ca17daf3138ff8d12a7bc89aa4a9
Raju Choudhary <awesomedesign007@gmail.com>
2019-11-27 19:50:04 +0530

    installed mongoose and connected to database

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 const app = express();
-
-//Route files
-const doctors = require('./routes/doctors');
 
 //Load env variables
 dotenv.config({ path: './config/config.env' });
+
+//Connect to DB
+connectDB();
+
+//Route files
+const doctors = require('./routes/doctors');
 
 //Dev logging Middleware
 if (process.env.NODE_ENV === 'development') {
@@ -20,7 +24,7 @@ app.use('/api/v1/doctors', doctors);
 
 //Start Server on PORT || 5000
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(
     `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
       .bold

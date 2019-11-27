11b901283bec166b4e0c6a0809d8e2a191430a89
Raju Choudhary <awesomedesign007@gmail.com>
2019-11-27 18:25:38 +0530

    update project name to careplus

--- server.js
+++ server.js
@@ -6,6 +6,39 @@ const app = express();
 //Load env variables
 dotenv.config({ path: './config/config.env' });
 
+//API
+//get all doctors
+app.get('/api/v1/doctors', (req, res) => {
+  res.status(200).json({ success: true, msg: 'show all doctros' });
+});
+
+//get single doctor wih id
+app.post('/api/v1/doctors/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Show single doctor porfile' });
+});
+
+//register new doctor
+app.post('/api/v1/doctors', (req, res) => {
+  res.status(200).json({ success: true, msg: 'regsiter new doctor' });
+});
+
+//update doctor profile wit id
+app.put('/api/v1/doctors/:id', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: `Updated doctor profile id: ${req.params.id}`
+  });
+});
+
+//delete doctor profile with id
+app.delete('/api/v1/doctors/:id', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: `Deleted doctor profile id: ${req.params.id}`
+  });
+});
+
+//Start Server on PORT || 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(
